d1f7bcf8c121876ecd6fed3e2974e4ea6abb3aef
James Meyers <jmmeyers215@gmail.com>
2023-11-17 16:56:09 -0700

    Added UI Clarity

--- Scripts/MainMenuController.gd
+++ Scripts/MainMenuController.gd
@@ -7,10 +7,16 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_released("one_button"):
 		id_select += 1
 		select_counter = 0
+		$Buttons/PlayBar.value = 0
+		$Buttons/OptionsBar.value = 0
+		$Buttons/ExitBar.value = 0
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("one_button"):
 		select_counter += 1
+		$Buttons/PlayBar.value = select_counter
+		$Buttons/OptionsBar.value = select_counter
+		$Buttons/ExitBar.value = select_counter
 	
 	if select_counter == 30 && id_select == 1:
 		get_tree().change_scene_to_file("res://Scenes/main.tscn")
@@ -28,14 +34,23 @@ func _physics_process(delta: float) -> void:
 		$Buttons/PlayHover.visible = true
 		$Buttons/OptionsHover.visible = false
 		$Buttons/ExitHover.visible = false
+		$Buttons/PlayBar.visible = true
+		$Buttons/OptionsBar.visible = false
+		$Buttons/ExitBar.visible = false
 	elif id_select == 2:
 		$Buttons/PlayHover.visible = false
 		$Buttons/OptionsHover.visible = true
 		$Buttons/ExitHover.visible = false
+		$Buttons/PlayBar.visible = false
+		$Buttons/OptionsBar.visible = true
+		$Buttons/ExitBar.visible = false
 	elif id_select == 3:
 		$Buttons/PlayHover.visible = false
 		$Buttons/OptionsHover.visible = false
 		$Buttons/ExitHover.visible = true
+		$Buttons/PlayBar.visible = false
+		$Buttons/OptionsBar.visible = false
+		$Buttons/ExitBar.visible = true
 
 
 
--- Scripts/UIController.gd
+++ Scripts/UIController.gd
@@ -13,6 +13,8 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_pressed("one_button") && main.paused == true:
 		select_counter += 1
+		$PausePanel/QuitBar.value = select_counter
+		$PausePanel/ExitBar.value = select_counter
 
 	if select_counter == 30 && id_select == 1:
 		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
@@ -22,6 +24,8 @@ func _physics_process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_released("one_button") && main.paused == true:
 		select_counter = 0
+		$PausePanel/QuitBar.value = 0
+		$PausePanel/ExitBar.value = 0
 		if id_select == 1:
 			id_select = 2
 		elif id_select == 2:
@@ -31,6 +35,10 @@ func hover(id_select):
 	if id_select == 1:
 		$PausePanel/QuitHover.visible = true
 		$PausePanel/ExitHover.visible = false
+		$PausePanel/QuitBar.visible = true
+		$PausePanel/ExitBar.visible = false
 	elif id_select == 2:
 		$PausePanel/ExitHover.visible = true
 		$PausePanel/QuitHover.visible = false
+		$PausePanel/QuitBar.visible = false
+		$PausePanel/ExitBar.visible = true
