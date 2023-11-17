09abffdaf27e9c1634dfeb0c0c6d3eab431b9173
James Meyers <jmmeyers215@gmail.com>
2023-11-17 13:34:52 -0700

    Movement and Charge implemented

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -2,8 +2,14 @@ extends RigidBody2D
 
 class_name Bullet
 
-@export var bullet_speed : int = 1
+@export var bullet_speed : int = 500
+@export var bullet_size : int = 1
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready() -> void:
+	$Sprite2D.scale = Vector2(bullet_size, bullet_size)
+	$CollisionShape2D.scale =  Vector2(bullet_size, bullet_size)
+
 func _process(delta: float) -> void:
-	transform.x += Vector2(bullet_speed,0)
+	translate(Vector2(bullet_speed*delta,0))
+	if global_position. x > 1280:
+		queue_free()
--- Scripts/ship.gd
+++ Scripts/ship.gd
@@ -2,24 +2,49 @@ extends Node2D
 
 class_name Ship
 var charge_count : int = 1
+@export var small_bullet_scene : PackedScene 
+@export var big_bullet_scene : PackedScene
+@export var ship_speed : float = 1.0
+var ship_up : bool = false
+
+func _ready() -> void:
+	$AnimatedSprite2D.play()
 
 func _physics_process(delta: float) -> void:
-	move_ship()
-
-func move_ship():
-	pass
-
-func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed("one_button"):
 		charge_count += 1
-		if charge_count > 100:
-			charge_count = 100
+		if charge_count > 50:
+			charge_count = 50
 	
-	if Input.is_action_just_released("one_button"):
-		fire_bullet()
+	move_ship()
+	$ChargeBar.value = charge_count
+	if ship_up == true:
+		position.y -= ship_speed
+	elif ship_up == false:
+		position.y += ship_speed
 
-func fire_bullet():
-	if charge_count > 50 && charge_count < 100:
-		pass
-	elif charge_count == 100:
-		pass
+func move_ship():
+	if position.y >= 600:
+		ship_up = true
+	elif position.y <= 50:
+		ship_up = false
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_released("one_button") &&  charge_count == 50:
+		var big_b = big_bullet_scene.instantiate()
+		fire_big_bullet(big_b)
+		charge_count = 0
+	elif Input.is_action_just_released("one_button") && charge_count < 50 && charge_count > 25:
+		var small_b = small_bullet_scene.instantiate()
+		fire_small_bullet(small_b)
+		charge_count = 0
+	elif Input.is_action_just_released("one_button"):
+		charge_count = 0
+
+func fire_big_bullet(big_b):
+	owner.add_child(big_b)
+	big_b.position = $".".global_position
+
+func fire_small_bullet(small_b):
+	owner.add_child(small_b)
+	small_b.position = $".".global_position
