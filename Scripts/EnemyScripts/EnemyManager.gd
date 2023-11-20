eb5232f556d31507dad90ce87cdced7b6788853d
James Meyers <jmmeyers215@gmail.com>
2023-11-19 23:49:36 -0700

    enemy management work

--- Scripts/EnemyScripts/EnemyManager.gd
+++ Scripts/EnemyScripts/EnemyManager.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-
+var drone : Enemy
+var elite : Enemy
+var boss : Enemy
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
